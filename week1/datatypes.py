79807024fce74a1996eae3f8eb11bb215e1b0e91
prasanna-M-24 <prasannameesalah9@gmail.com>
2025-12-10 21:59:57 +0530

    first code

---
+++ week1/datatypes.py
@@ -0,0 +1,13 @@
+n1=5
+str1="prasanna"
+n2=12.6
+char='p'
+print("num1: ",n1,"type of n1: ",type(n1))
+print("str1: ",str1,"type of str1: ",type(str1))
+print("num2: ",n2,"type of n2: ",type(n2))
+print("char: ",char,"type of char: ",type(char))
+print("result: ",n2/n1,"type of result: ",type(n2/n1))
+print("value: ",ord(char),"type of value: ",type(ord(char)))
+print("total length of a string: ",len(str1))
+print("slice of string: ",str1[0:4])
+print("floor rounding: ",n2//n1)
