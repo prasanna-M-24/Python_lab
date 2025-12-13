5a4df400e723c5749d297027e482ea1e8c33dc23
prasanna-M-24 <prasannameesalah9@gmail.com>
2025-12-13 17:04:23 +0530

    calculated simple intrest

---
+++ week2/simpleintrest.py
@@ -0,0 +1,5 @@
+p=float(input("Enter the principal amount: "))
+t=float(input("Enter the time in years: "))
+r=float(input("Enter the rate of interest: "))
+si=(p*t*r)/100
+print("Simple Interest is: ",si)
